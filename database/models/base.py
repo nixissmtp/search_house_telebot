fb3a37df35453df68e7d1d240026060d78a8de2a
nixissmtp <gleb@simple2b.(none)>
2022-03-09 06:53:25 +0200

    remove same thread

--- database/models/base.py
+++ database/models/base.py
@@ -3,5 +3,5 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
-engine = create_engine('sqlite:///bot.db')
+engine = create_engine('sqlite:///bot.db', connect_args={'check_same_thread': False})
 Session = sessionmaker(bind=engine)
